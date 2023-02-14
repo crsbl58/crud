03b2977bb25e20b93bfb0f171e8c470e44d1a067
cortega <cortega@acciontip.cl>
2023-02-14 03:51:25 -0300

    crud

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header"><h1>Crud</h1></header>
       <main>
         <div>
           <div className="flexRow">
